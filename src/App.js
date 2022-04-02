dc6dd4cb802b23290591133edac30b50bae56a63
Vishal Kumar <vishalsingh9632@gmail.com>
2022-04-02 12:08:06 +0530

    Feat : Add Counter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { incNumber, decNumber } from './actions/actions';
 
 function App() {
+  const myState = useSelector((state) => state.changeTheNumber);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <header className="App-header">
         Counter Using Redux
-          <div className="todo">
-            <div className="todo-buttons">-</div>
-            <div className="todo-buttons">0</div>
-            <div className="todo-buttons">+</div>
-          </div>
+        <div className="todo">
+          <div className="todo-buttons"
+            onClick={() => dispatch(decNumber())}
+          >-</div>
+          <div className="todo-buttons">{myState}</div>
+          <div className="todo-buttons"
+            onClick={() => dispatch(incNumber())}
+          >+</div>
+        </div>
       </header>
     </div>
   );
---
+++ src/reducers/reducers.js
@@ -0,0 +1,9 @@
+import changeTheNumber from "./upDown";
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+    changeTheNumber,
+    // changeTheNumber
+})
+
+export default rootReducer;
